6c1c4de46313c4ec18778899970d984290615a4d
MohammedKhalidEk <mohamednaser.mn91@gmail.com>
2018-10-25 19:40:52 +0200

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -1,17 +1,36 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
-var app = express();
 const port = process.env.PORT || 3000;
 
-// app.get('/', (req, res) => {
-//     res.send('Chat app!');
-// });
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// listen to new connections
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: "mohamednaser@gmail.com",
+        text: "Hey",
+        createdAt: 12
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+}); 
+
+server.listen(port, () => {
     console.log(`app listening on port ${port}`);
 });
